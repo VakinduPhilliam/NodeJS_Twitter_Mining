28a23160c129901bfd9d4dd0ad3e65267df72b3e
Vakindu Philliam <Pvakindu@gmail.com>
2019-02-13 16:13:49 -0600

    Add files via upload

---
+++ NodeJS_Twitter_Bot.js
@@ -0,0 +1,40 @@
+//Build a simple Twitter Bot with Node.js in just 38 lines of code
+
+var Twitter = require('twitter');
+var config = require('./config.js');
+var T = new Twitter(config);
+
+// Set up your search parameters
+var params = {
+  q: '#nodejs',
+  count: 10,
+  result_type: 'recent',
+  lang: 'en'
+}
+
+// Initiate your search using the above paramaters
+T.get('search/tweets', params, function(err, data, response) {
+  // If there is no error, proceed
+  if(!err){
+    // Loop through the returned tweets
+    for(let i = 0; i < data.statuses.length; i++){
+      // Get the tweet Id from the returned data
+      let id = { id: data.statuses[i].id_str }
+      // Try to Favorite the selected Tweet
+      T.post('favorites/create', id, function(err, response){
+        // If the favorite fails, log the error message
+        if(err){
+          console.log(err[0].message);
+        }
+        // If the favorite is successful, log the url of the tweet
+        else{
+          let username = response.user.screen_name;
+          let tweetId = response.id_str;
+          console.log('Favorited: ', `https://twitter.com/${username}/status/${tweetId}`)
+        }
+      });
+    }
+  } else {
+    console.log(err);
+  }
+})
